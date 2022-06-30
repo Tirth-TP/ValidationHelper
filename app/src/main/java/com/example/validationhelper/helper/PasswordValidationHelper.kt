c6baf663fbd5dc867778cdae3bddbe9d71a13256
Tirth <tirthp.itpath@gmail.com>
2022-06-30 15:03:13 +0530

    DONE : - [x] Update in Password pattern

--- app/src/main/java/com/example/validationhelper/helper/PasswordValidationHelper.kt
+++ app/src/main/java/com/example/validationhelper/helper/PasswordValidationHelper.kt
@@ -7,13 +7,13 @@ import android.widget.EditText
  */
 fun EditText.passwordValidationHelper(){
     val password = this.text.toString()
-    val passwordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$"
+    val passwordPattern = "^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$%^&+=!])(?=\\S+$).{8,}$"
 
     if(password.isNotEmpty()) {
         if (password.matches(passwordPattern.toRegex())){
             return
         }else{
-            this.error = "Password must contains \n 1 uppercase \n 1 number \n 1 special character \n and min length of 8"
+            this.error = "Password must contains \n 1 uppercase \n 1 lowercase \n 1 number \n 1 special character \n and min length of 8"
         }
     }else{
         this.error = "This field is required"
