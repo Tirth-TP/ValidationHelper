2687d37395b5fba6f14269e0a23416248cb1522a
Tirth <tirthp.itpath@gmail.com>
2022-07-01 17:57:06 +0530

    DONE : - [x] Update in Email pattern

--- app/src/main/java/com/example/validationhelper/helper/EmailValidationHelper.kt
+++ app/src/main/java/com/example/validationhelper/helper/EmailValidationHelper.kt
@@ -1,6 +1,7 @@
 package com.example.validationhelper.helper
 
 import android.widget.EditText
+import java.util.regex.Pattern
 
 /**
  * Created by Tirth Patel.
@@ -8,7 +9,16 @@ import android.widget.EditText
 
 fun EditText.emailValidationHelper() {
     val email = this.text.toString()
-    val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+//    val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
+    val emailPattern =   Pattern.compile(
+        "[a-zA-Z0-9\\+\\.\\!\\_\\%\\-\\+\\#\\$\\&\\'\\*\\/\\=\\?\\^\\`\\{\\|\\}\\~]{1,256}" +
+                "\\@" +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
+                "(" +
+                "\\." +
+                "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
+                ")+"
+    )
     if (email.isNotEmpty()) {
         if (email.matches(emailPattern.toRegex())) {
             return
