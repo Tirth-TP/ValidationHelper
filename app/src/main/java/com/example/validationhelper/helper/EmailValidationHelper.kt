f896248279bfe140bc25c9f401c491a944e0e017
Tirth <tirthp.itpath@gmail.com>
2022-07-05 10:45:19 +0530

    DONE : - [x] Update in Email pattern

--- app/src/main/java/com/example/validationhelper/helper/EmailValidationHelper.kt
+++ app/src/main/java/com/example/validationhelper/helper/EmailValidationHelper.kt
@@ -11,7 +11,7 @@ fun EditText.emailValidationHelper() {
     val email = this.text.toString()
 //    val emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+"
     val emailPattern =   Pattern.compile(
-        "[a-zA-Z0-9\\+\\.\\!\\_\\%\\-\\+\\#\\$\\&\\'\\*\\/\\=\\?\\^\\`\\{\\|\\}\\~]{1,256}" +
+         "[a-zA-Z0-9\\-]"+"[a-zA-Z0-9\\+\\.\\!\\_\\%\\-\\+\\#\\$\\&\\'\\*\\/\\=\\?\\^\\`\\{\\|\\}\\~]{1,256}" +
                 "\\@" +
                 "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}" +
                 "(" +
